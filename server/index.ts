d9e2b4e9e873870b3b2377c8cf603f766a9f47c3
negyeliczkysandor <sandor.negyeliczky@gmail.com>
2023-12-02 19:27:27 +0100

    add: zod validation

--- server/index.ts
+++ server/index.ts
@@ -3,11 +3,24 @@ import {createHTTPServer} from "@trpc/server/adapters/standalone";
 import {z} from "zod";
 import prisma from "@/prisma/db";
 
+const Status = z.enum(["IN_CART", "PAYED", "REMUVED"])
 
+
+const productCart = z.object({
+    name: z.string(),
+    price: z.number(),
+    status: Status,
+    userId: z.string(),
+    productId: z.string()
+})
+// add zod validation
 export const appRouter = router({
     allCart: publicProcedure.query(async () => {
         const p = await prisma.product.findMany()
         return p
+    }),
+    addProduct: publicProcedure.input(productCart).mutation(async (opts) => {
+        const {input} = opts
     })
 });
 
