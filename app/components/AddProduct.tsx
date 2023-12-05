726f82f799eae93a91248b8b3c8ad28538ada3e0
negyeliczkysandor <sandor.negyeliczky@gmail.com>
2023-12-05 10:20:34 +0100

    add: button disabling on adding and loading

--- app/components/AddProduct.tsx
+++ app/components/AddProduct.tsx
@@ -5,21 +5,23 @@ import {Product} from "@/app/types";
 import React, {FC} from "react";
 import {serverClient} from "@/app/_trpc/serverClient";
 import Button from "@/app/components/uiComponents/Button";
+import Spinner from "@/app/components/uiComponents/Spinner";
 
 type props = {
     product: Product
     initStockAmount: Awaited<ReturnType<(typeof serverClient)["getStockForProduct"]>>
 }
 const AddProduct: FC<props> = ({product, initStockAmount}) => {
-    
-    const getStack = trpc.getStockForProduct.useQuery({prodId: product.id}, {
+
+    const getStock = trpc.getStockForProduct.useQuery({prodId: product.id}, {
         initialData: initStockAmount,
     })
+
     const addToCart = trpc.addProduct.useMutation({
-        onSettled: () => getStack.refetch()
+        onSettled: () => getStock.refetch()
     })
 
-    const amount = getStack.data?.quantity
+    const amount = getStock.data?.quantity
 
     const addProduct = async () => await addToCart.mutate({
         productId: product.id,
@@ -29,12 +31,13 @@ const AddProduct: FC<props> = ({product, initStockAmount}) => {
         userId: "1"
     })
 
-    return (
-        <>
-            <p className="mt-1 text-lg font-medium text-gray-900">Available: {amount}</p>
-            <Button isDisabled={!amount || amount <= 0} onclick={addProduct} text={"Add product"}/>
-        </>
-    );
+    const isAdding = getStock.isLoading || addToCart.isLoading
+
+    return (<>
+        <p className="mt-1 text-sm font-sm text-gray-900">Available: {amount}</p>
+        <Button isDisabled={(!amount || amount <= 0) || isAdding} onclick={addProduct} text={"Add product"}/>
+        {isAdding && <Spinner/>}
+    </>)
 };
 
 export default AddProduct;
